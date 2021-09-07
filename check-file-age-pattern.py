1e03ee5e08330139403654b20e92012624bc5aff
Benjamin <benji81@gmail.com>
2021-09-07 14:55:32 +0200

    feat: add --all flag to check all files

--- check-file-age-pattern.py
+++ check-file-age-pattern.py
@@ -17,12 +17,75 @@ parser.add_argument(
     help="files pattern (give them between '' if you use *",
 )
 parser.add_argument(
-    "-w", "--warning", dest="warning", type=int, default=86400, help="TODO"
+    "-w",
+    "--warning",
+    dest="warning",
+    type=int,
+    default=86400,
+    help="minimum modification time in seconds to raise a warning",
 )
 parser.add_argument(
-    "-c", "--critical", dest="critical", type=int, default=86400 * 2, help="TODO"
+    "-c",
+    "--critical",
+    dest="critical",
+    type=int,
+    default=86400 * 2,
+    help="minimum modification time in seconds to raise a critical",
 )
+
+parser.add_argument(
+    "-a",
+    "--all",
+    dest="all",
+    action="store_true",
+    help="All files found must match minimal modification time",
+)
+
 current_time = time.time()
+
+
+def check_all(age_path_tuple_list):
+    # Need to check for a critical then we can test warnings.
+    for age, path in age_path_tuple_list:
+        if age > args.critical:
+            # f-string not possible because of compatibility versions
+            print(
+                path
+                + " modified "
+                + str(datetime.timedelta(seconds=int(age)))
+                + " ago."
+            )
+            sys.exit(2)
+    for age, path in age_path_tuple_list:
+        if age > args.warning:
+            # f-string not possible because of compatibility versions
+            print(
+                path
+                + " modified "
+                + str(datetime.timedelta(seconds=int(age)))
+                + " ago."
+            )
+            sys.exit(1)
+    print("All files are recent enough")
+    sys.exit(0)
+
+
+def check_min(age_path_tuple_list):
+    min_path_age = min(age_path_tuple_list, key=lambda age: age[0])
+    # f-string not possible because of compatibility versions
+    print(
+        "Most recent file is "
+        + min_path_age[1]
+        + " modified "
+        + str(datetime.timedelta(seconds=int(min_path_age[0])))
+        + " ago."
+    )
+
+    if min_path_age[0] > args.critical:
+        sys.exit(2)
+    if min_path_age[0] > args.warning:
+        sys.exit(1)
+    sys.exit(0)
 
 
 if __name__ == "__main__":
@@ -33,23 +96,13 @@ if __name__ == "__main__":
             (current_time - os.path.getmtime(path), path)
             for path in chain.from_iterable(globs)
         ]
-        min_path_age = min(age_path_tuple_list, key=lambda age: age[0])
-        # f-string not possible because of compatibility versions
-        print(
-            "Most recent file is "
-            + min_path_age[1]
-            + " modified "
-            + str(datetime.timedelta(seconds=int(min_path_age[0])))
-            + " ago."
-        )
         if not age_path_tuple_list:
             print("No file found in specified path(s)")
             sys.exit(3)
-        if min_path_age[0] > args.critical:
-            sys.exit(2)
-        if min_path_age[0] > args.warning:
-            sys.exit(1)
-        sys.exit(0)
+        if args.all:
+            check_all(age_path_tuple_list)
+        else:
+            check_min(age_path_tuple_list)
     except Exception as e:
         print(e)
         sys.exit(3)
